50c847a2485badea38c6d49123820df198d21ac6
chaimaa <132452785+19940102@users.noreply.github.com>
2024-02-24 15:08:19 +0100

    Add files via upload

---
+++ GestionProduit.java
@@ -0,0 +1,25 @@
+package exr1.cl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionProduit {
+    public static void main(String[] args) {
+
+    List<produit> produits=new ArrayList<>();
+    produits.add(new produit(1,"HP",200000));
+    produits.add(new produit(2,"DEL",1800000));
+    produits.add(new produit(3,"MAC",150000));
+
+    produits.remove(0);
+    for (produit p:produits) {
+        System.out.println(p);
+    }
+    System.out.println("******* Q4 ********");
+produits.get(0).setPrix(250000);
+for (produit p:produits) {
+System.out.println(p);
+
+}
+}
+}
